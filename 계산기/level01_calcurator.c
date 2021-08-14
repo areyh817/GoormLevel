c3348ea7b21dde3efdf7b48c3544ecad0af18d47
CHO HYE RA <s2037@e-mirim.hs.kr>
2021-08-14 15:37:21 +0900

    계산을 해주는 프로그램

---
+++ 계산기/level01_calcurator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+	int num1, num2;
+	char ch;
+	
+	scanf("%d %c %d", &num1, &ch, &num2);
+	
+	switch(ch){
+		case '+': printf("%d", num1 + num2); break;
+		case '-': printf("%d", num1 - num2); break;
+		case '*': printf("%d", num1 * num2); break;
+		case '/': printf("%d", num1 / num2); break;
+	}
+	return 0;
+}
